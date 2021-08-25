0da90c3d1b2641abc8c9716f55b137271a0b1a4e
Jisu Kim <klog104@kaist.ac.kr>
2021-08-25 13:35:40 +0900

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,10 +1,15 @@
 import gradio
 
 def hello(inp):
-  return "Hello!"
+#   return "Hello!"
+  return inp
 
 image = gradio.inputs.Image(label="Input image", source="webcam")
 
-io = gradio.Interface(fn=hello, live=True, inputs=image, outputs='text', title='Hello World', 
+# io = gradio.Interface(fn=hello, live=True, inputs=image, outputs='text', title='Hello World', 
+#     description='The simplest Hosted interface.')
+
+io = gradio.Interface(fn=hello, live=True, inputs=image, outputs=image, title='Hello World', 
     description='The simplest Hosted interface.')  
+
 io.launch()
