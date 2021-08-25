1c4a44dc87528ba78fcc47b5d6642410d14739e8
Jisu Kim <klog104@kaist.ac.kr>
2021-08-25 11:32:36 +0900

    Create __init__.py

---
+++ src/models/backbones/__init__.py
@@ -0,0 +1,9 @@
+from .wrapper import *
+#------------------------------------------------------------------------------
+#  Replaceable Backbones
+#------------------------------------------------------------------------------
+
+SUPPORTED_BACKBONES = {
+    'mobilenetv2': MobileNetV2Backbone,
+    'mobilenetv3': MobileNetV3Backbone,
+}
